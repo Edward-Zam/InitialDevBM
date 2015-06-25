22ecf6cf691f982abba6c81a2fb5aace34df132e
Edward-Zam <edward.zamora@outlook.com>
2015-06-25 12:59:42 -0700

    Update CheckGitRepo.sh
    Modified file to check git repository for changes. Download changes if available. Outputs enabled.

--- ShellScripts/CheckGitRepo.sh
+++ ShellScripts/CheckGitRepo.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+git fetch origin
+
+#See if there are any incoming changes
+git log HEAD..origin/master --oneline
+#Pull if changes
+if [[ "${reslog}" != "" ]] ; then
+  echo "Update Available. Downloading."
+  git merge origin/master #completing the pull
+else 
+  echo "Current Version Already Installed."
+fi
+
