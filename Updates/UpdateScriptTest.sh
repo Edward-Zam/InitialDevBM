5fb4c591dbb4f7f89f1bd8914ed8105bccc1dfb1
Edward Zamora <edward.zamora@outlook.com>
2015-06-25 17:40:12 -0700

    Added UpdateScriptTest. This will be the script which will run the update. Currently checks new version against old version, if they are different it will update the old one and reboot. This file will contain paths for other scripts to run to conduct changes and updates

---
+++ Updates/UpdateScriptTest.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+VERSION=V0
+# This script will run after an update has been downloaded from the repo. It must check for changes in the update file or in version number. Update script will have version number on first line in order to compare easily.
+
+# Read old file to check version. Store in OLDVERSION.
+
+#Compare Versions using string comparisons. Will only check if different not if different not if older or newer.
+OLDVERSION=$(sed -n '2p' ~/Updates/UpdateScriptTest.sh)
+echo $VERSION
+echo ${OLDVERSION:8}
+
+if [ ${OLDVERSION:8} == $VERSION ]; then
+	echo "Software is up to date. No need to install."
+else
+	echo "New Version Available. Installing."
+	#Do other things here. Such as write to file, over write, run script.
+	#Currently set to reboot after 10 mins. This gives time for everything to complete.
+	cp -f ~/InitialDevBM/Updates/UpdateScriptTest.sh ~/Updates/UpdateScriptTest.sh
+	sudo shutdown -r 10 "Rebooting in 10 minutes to apply Update."
+fi
+
+
